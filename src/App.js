5bb346f48b10797aabf25fbee2f9fdded4493363
pikachungg <34949943+pikachungg@users.noreply.github.com>
2019-12-18 10:46:04 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="wrapper">
+		<div className="column1">
+
+		</div>
+		<div className="column2">
+
+		</div>
+    </div>
+  );
+}
+
+export default App;
