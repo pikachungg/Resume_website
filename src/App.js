8d40ca9933bba8d7e1fb97cf85c59aaea7381313
pikachungg <34949943+pikachungg@users.noreply.github.com>
2019-12-19 09:35:54 -0500

    column1 for web

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import me from './assets/images/resume.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="wrapper">
+		<link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700,900&display=swap" rel="stylesheet"></link>
 		<div className="column1">
-
+			<img src={me} alt="a picture of me"/>
+			<div className="center">
+				<h3>Hi, I'm Daniel Chung</h3>
+				<p>A Web developer aiming to create beautiful and efficient websites and applications.</p>
+			</div>
 		</div>
 		<div className="column2">
 
