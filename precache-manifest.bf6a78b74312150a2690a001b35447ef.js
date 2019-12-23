9c40420283aa81f510d79c922673e875766c0bdd
pikachungg <34949943+pikachungg@users.noreply.github.com>
2019-12-23 01:52:46 -0500

    Updates

---
+++ precache-manifest.bf6a78b74312150a2690a001b35447ef.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "645305b01b36c9649fff082f9628f31d",
+    "url": "/Resume_website/index.html"
+  },
+  {
+    "revision": "dc591c764fbf62b074d2",
+    "url": "/Resume_website/static/css/main.667d4d01.chunk.css"
+  },
+  {
+    "revision": "e4162f51a8403b5d0d34",
+    "url": "/Resume_website/static/js/2.026d829d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Resume_website/static/js/2.026d829d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dc591c764fbf62b074d2",
+    "url": "/Resume_website/static/js/main.7fc02903.chunk.js"
+  },
+  {
+    "revision": "27ff5f533d6ba1c09456",
+    "url": "/Resume_website/static/js/runtime-main.96253d35.js"
+  },
+  {
+    "revision": "ad92cbf5d41ae97890b3e983c5d0ce77",
+    "url": "/Resume_website/static/media/github.ad92cbf5.png"
+  },
+  {
+    "revision": "842a12927e6a59379005cfe6ba146b56",
+    "url": "/Resume_website/static/media/instagram.842a1292.png"
+  },
+  {
+    "revision": "00d0738313dd762a60cbb4c5a6fad096",
+    "url": "/Resume_website/static/media/resume.00d07383.jpg"
+  },
+  {
+    "revision": "4864cee3fe38979765520ea9bf169922",
+    "url": "/Resume_website/static/media/website.4864cee3.png"
+  }
+]);
