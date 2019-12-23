9e0d15fbde94ded9b241969c942927c03cd4ff06
pikachungg <34949943+pikachungg@users.noreply.github.com>
2019-12-23 17:16:28 -0500

    Grammar checks

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Resume_website/precache-manifest.09fd05e5d345f192f640b30fb96cfbdd.js"
+  "/Resume/precache-manifest.2babfce44cd127c3e024fec844970011.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Resume_website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Resume/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
--- src/App.js
+++ src/App.js
@@ -38,10 +38,11 @@ function App() {
 				<div className="space"></div>
 				{/* Introduction */}
 				<Content title="Intro:"
-				paragraph1="I'm a Web developer born and raised in Panama. I'm currently on my third year pursuing a degree in Web and Mobile Computing at Rochester Institute of Technology."
-				paragraph2="I enjoy making people's ideas into reality. Whether it is an application or a Website I love working with people to create something from start to fnish."
-				paragraph3="I'm currently looking for a Co-Op in Summer 2020. Most experience in front-end or back-end development."
+				paragraph1="Hi! I'm a Web developer born and raised in Panama with Asian roots. I'm currently in my third year pursuing a degree in Web and Mobile Computing at Rochester Institute of Technology (RIT)."
+				paragraph2="I specialize in building websites and applications with easy to use and modern user interfaces and back-ends. I enjoy developing both front-end and back-end for all my sites and apps."
+				paragraph3="I'm currently looking for a Co-Op in summer 2020."
 				/>
+				<div className="space"></div>
 				{/* Education */}
 				<Education title="Education:"
 				university="Rochester Institute of Technology"
@@ -52,7 +53,7 @@ function App() {
 				Minor="Computer Science"
 				expected="Expected Graduation: Spring 2021"
 				/>
-
+				<div className="space"></div>
 				{/* TOOLS */}
 				<Tools title="Tools: "
 				languages="LANGUAGES: "
@@ -62,42 +63,42 @@ function App() {
 				design="DESIGN: "
 				designAll="Adobe Photoshop, Adobe XD"
 				/>
-
+				<div className="space"></div>
 				{/* WORK */}
 				<Work title="Work: "
 				place="bbb Shoes & Boots"
 				position="Social Media Intern"
 				duration="Jan 2016 - Aug 2016"
-				description="Responsible for taking and editing pictures of the company’s products for use in displays and on social media. Edited with Photoshop."
+				description="I photographed the company's products, edited the pictures, and introduced new creative ideas for social media posts. I used my pictures for the company's website and social media. I adapted and learned photoshop."
 				/>
-
+				
 				<Work title=""
 				place="The Commons"
 				position="Line Cook"
 				duration="Sep 2017 - Aug 2016"
-				description="Worked efficiently under pressure during rush hour shifts. Implemented a faster way to close and clean at the end of the day."
+				description="Worked efficiently under pressure during the rush hour shifts and implemented a faster way for closing and cleaning at the end of the day."
 				/>	
 
 				<Work title=""
 				place="bbb Shoes and Boots"
 				position="Software Developer"
 				duration="May 2019 - Aug 2019"
-				description="Developed four web applications that helped improve the company's storage system. Using MySQL, PHP, HTML/CSS."
+				description="I built four useful applications for the company to facilitate the communication between the information in their database and the user's experience for fetching such information. I used HTML/CSS, JavaScript, and PHP."
 				/>
-
+				<div className="space"></div>
 				{/* project */}
 				<Work title="Projects:"
 				place="Personal Project"
 				position="Discordgg"
 				duration="In development"
-				description="Discord bot that retrieves information from the Riot Games’ League of Legends API and displays it on discord. Helps you keep track of player's stats and current game information. Developing with Python."
+				description="A Discord bot that retrieves information from the Riot Games' League of Legends API and displays user requested information on discord. It helps you keep track of the player's stats and current game information, depending on what its asked. This bot is currently being developing with Python."
 				/>
 
 				<Work title=""
 				place="Project"
 				position="Swoop"
 				duration="In development"
-				description="Game developed aimed to reduce stress and for mental health. This game was developed using HTML/CSS, javascript and implemented ReactJS."
+				description="Swoop is a game developed with mental health as our primary goal. We aimed to reduce stress and anxiety through a relaxing and engaging experience. This project developed using HTML/CSS, JavaScript, and ReactJS."
 				/>	
 
 				<Work title=""
@@ -108,12 +109,12 @@ function App() {
 				/>	
 				
 				{/* Leadership */}
-
+				<div className="space"></div>
 				<Work title="Leadership:"
 				place="Squad Leader"
 				position="TECHO Panama"
 				duration="Aug 2015 - Present"
-				description="Developed leaderships skill through actively cooperating to society trying to diminish poverty in Panama by building houses in low access places. I've lead more than 9 teams in building houses."
+				description="Developed leaderships skill through actively cooperating to society, trying to diminish poverty in Panama by building houses in low access places. I've led more than 9 teams in building houses."
 				/>
 
 				{/* FOOTER */}
